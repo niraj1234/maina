95651dd030595bb551fbc7d1716c8c8a58b404ca
Niraj <niraj.webtech@gmail.com>
2022-12-20 12:56:29 +0530

    Application Style changed Now WAR file | Thymeleaf | Lombok

---
+++ src/main/java/com/niraj/maina/controller/Person.java
@@ -0,0 +1,53 @@
+package com.niraj.maina.controller;
+
+
+
+public class Person {
+
+	private String name;
+	private String email;
+	private String mobile;	
+	
+	public Person() {
+		super();
+	}
+	
+	
+	public Person(String name, String email, String mobile) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.mobile = mobile;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", email=" + email + ", mobile=" + mobile + "]";
+	}
+
+
+	
+	
+	
+}
